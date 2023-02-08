24289cc23d0ff3558ab69a71b3945243cd5da076
Tim <timothee.varaillas@gmail.com>
2023-02-08 09:56:40 +0100

    + Initialisation de l'application Vue 3 + Vite

---
+++ webapp/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header></header>
+
+  <main>
+    <h2 class="text-2xl font-bold text-blue-800">
+      Project with Vite, Vue 3 & Tailwind CSS
+    </h2>
+  </main>
+</template>
+
+<style scoped></style>

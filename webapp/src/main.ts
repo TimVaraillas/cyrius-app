a0bed19914fb24cc06a4f4457207158065a9d001
Tim <timothee.varaillas@gmail.com>
2023-02-10 12:36:44 +0100

    + Mise à jour d fichier main.ts

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -4,4 +4,21 @@ import App from "./App.vue";
 import "./assets/main.css";
 import "./assets/tailwind.css";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faEye,
+  faEdit,
+  faTrash,
+  faXmark,
+  faCheck,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEye, faEdit, faTrash, faXmark, faCheck, faChevronDown);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
